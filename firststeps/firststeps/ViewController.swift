8f15bfc1466075693ceca42092f2ed290ca48969
ITESM CAMPUS TAMPICO <Administrador@Graphic-design07.local>
2016-03-05 10:33:00 -0600

    First Commit
    A Label was created in Xcode for iOS running natively on MACOS X Yosemite.

---
+++ firststeps/firststeps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firststeps
+//
+//  Created by ITESM CAMPUS TAMPICO on 05/03/16.
+//  Copyright © 2016 Iván García. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
